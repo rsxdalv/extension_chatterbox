30da0826f73ef525e70d53d69fcd584f57cf15e2
Roberts Slisans <rsxdalv@gmail.com>
2025-06-02 03:57:17 +0300

    add voice dropdown and chunked generation

--- extension_chatterbox/gradio_app.py
+++ extension_chatterbox/gradio_app.py
@@ -1,3 +1,4 @@
+import os
 import functools
 import gradio as gr
 from contextlib import contextmanager
@@ -13,6 +14,10 @@ from tts_webui.extensions_loader.decorator_extensions import (
 from tts_webui.utils.list_dir_models import unload_model_button
 from tts_webui.utils.manage_model_state import manage_model_state
 from tts_webui.utils.randomize_seed import randomize_seed_ui
+from tts_webui.utils.OpenFolderButton import OpenFolderButton
+from tts_webui.utils.get_path_from_root import get_path_from_root
+from tts_webui.utils.get_path_from_root import get_path_from_root
+from gradio_iconbutton import IconButton
 
 
 def chatterbox_to(model: ChatterboxTTS, device, dtype):
@@ -75,7 +80,9 @@ def tts(
     model_name="just_a_placeholder",
     device="cuda",
     dtype="float32",
-    seed=-1, # for fn signature
+    # hyperparameters
+    chunked=False,
+    seed=-1,  # for signature compatibility
     **kwargs,
 ):
     device = get_best_device() if device == "auto" else device
@@ -91,13 +98,30 @@ def tts(
                 "bfloat16": torch.bfloat16,
             }[dtype],
         ) as model:
-            wav = model.generate(
-                text,
-                audio_prompt_path=audio_prompt_path,
-                exaggeration=exaggeration,
-                cfg_weight=cfg_weight,
-                temperature=temperature,
-            )
+
+            def generate_chunk(text):
+                print(f"Generating chunk: {text}")
+                return model.generate(
+                    text,
+                    audio_prompt_path=audio_prompt_path,
+                    exaggeration=exaggeration,
+                    cfg_weight=cfg_weight,
+                    temperature=temperature,
+                )
+
+            if chunked:
+                from tts_webui.utils.split_text_functions import (
+                    split_and_recombine_text,
+                )
+
+                texts = split_and_recombine_text(text)
+                wavs = [generate_chunk(text) for text in texts]
+                wav = torch.cat([w.squeeze() for w in wavs], dim=0)
+                return {
+                    "audio_out": (model.sr, wav.cpu().numpy()),
+                }
+
+            wav = generate_chunk(text)
 
         return {
             "audio_out": (model.sr, wav.cpu().numpy().squeeze()),
@@ -124,6 +148,16 @@ def tts_decorated(*args, _type=None, **kwargs):
     return tts(*args, **kwargs)
 
 
+def get_voices():
+    voices_dir = get_path_from_root("voices")
+    results = ["select a voice to load it"] + [
+        (x, os.path.join(voices_dir, x))
+        for x in os.listdir(voices_dir)
+        if x.endswith(".wav")
+    ]
+    return results
+
+
 def ui():
     gr.HTML(
         """
@@ -133,8 +167,8 @@ def ui():
     <div style="flex: 1; min-width: 300px;">
       <h2>Model sizes:</h2>
       <ul style="list-style-type: disc; padding-left: 20px; margin-top: 0;">
-        <li><strong>t3_cfg.pt</strong> 1.06 GB</li>
-        <li><strong>s3gen.pt</strong> 1.06 GB</li>
+        <li><strong>t3_cfg.safetensors</strong> 2.13 GB</li>
+        <li><strong>s3gen.safetensors</strong> 1.06 GB</li>
       </ul>
     </div>
 
@@ -157,6 +191,7 @@ def ui():
         with gr.Column():
             text = gr.Textbox(label="Text")
             btn = gr.Button("Generate", variant="primary")
+            chunked = gr.Checkbox(label="Split prompt into chunks", value=False)
             exaggeration = gr.Slider(
                 label="Exaggeration (Neutral = 0.5, extreme values can be unstable)",
                 minimum=0,
@@ -169,8 +204,27 @@ def ui():
             temperature = gr.Slider(
                 label="Temperature", minimum=0.05, maximum=5, value=0.8
             )
+
+            with gr.Row():
+                voice_dropdown = gr.Dropdown(
+                    label="Audio Prompt",
+                )
+                with gr.Column():
+                    IconButton("refresh").click(
+                        fn=lambda: gr.Dropdown(choices=get_voices()),
+                        outputs=[voice_dropdown],
+                    )
+                    voices_dir = get_path_from_root("voices")
+                    OpenFolderButton(voices_dir, api_name="tortoise_open_voices")
+
             audio_prompt_path = gr.Audio(
                 label="Reference Audio", type="filepath", value=None
+            )
+
+            voice_dropdown.change(
+                lambda x: gr.Audio(value=x),
+                inputs=[voice_dropdown],
+                outputs=[audio_prompt_path],
             )
             seed, randomize_seed_callback = randomize_seed_ui()
 
@@ -214,6 +268,8 @@ def ui():
                 device: "device",
                 dtype: "dtype",
                 model_name: "model_name",
+                # hyperparameters
+                chunked: "chunked",
             },
             outputs={
                 "audio_out": audio_out,
